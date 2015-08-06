476a4e0c4035d1006f8a25dc69233e7d6f6beee7
Ken Stack <kenstack@yahoo.com>
2015-08-05 22:45:16 -0400

    add option to use raw as primary, calc raw deltabg and direction

--- nsapple/ViewController2.swift
+++ nsapple/ViewController2.swift
@@ -13,6 +13,15 @@ class ViewController2: UIViewController {
     @IBOutlet weak var urlbutton: UIButton!
     @IBOutlet weak var pebbleurl: UITextField!
     
+    @IBOutlet weak var primarydisplay: UISegmentedControl!
+    
+    @IBAction func primarydisplay(sender: AnyObject) {
+          var defaults: NSUserDefaults = NSUserDefaults(suiteName: "group.perceptus.nsapple")!
+        if primarydisplay.selectedSegmentIndex==0 {defaults.setObject("dex", forKey: "primarydisplay")} else
+        {defaults.setObject("raw", forKey: "primarydisplay")}
+        
+        defaults.synchronize()
+    }
     @IBAction func urlbuttontouch(sender: AnyObject) {
         var defaults: NSUserDefaults = NSUserDefaults(suiteName: "group.perceptus.nsapple")!
         let url=pebbleurl.text
@@ -34,8 +43,16 @@ class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
          var defaults: NSUserDefaults = NSUserDefaults(suiteName: "group.perceptus.nsapple")!
-        let url=defaults.objectForKey("pebbleurl") as! String
-        pebbleurl.text=url
+        if let url=defaults.objectForKey("pebbleurl") as? String {
+            pebbleurl.text=url}
+        //will write dex to defaults first time thru
+        if let primarydisplay=defaults.objectForKey("primarydisplay") as? String {
+            if primarydisplay=="dex" {self.primarydisplay.selectedSegmentIndex=0} else {self.primarydisplay.selectedSegmentIndex=1}
+        }
+        if primarydisplay.selectedSegmentIndex==0 {defaults.setObject("dex", forKey: "primarydisplay")} else
+        {defaults.setObject("raw", forKey: "primarydisplay")}
+        defaults.synchronize()
+
 
         // Do any additional setup after loading the view.
     }
