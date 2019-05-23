c2dc18e13a923cad6e3ee3cf9a22214907f9a683
Ken Stack <kenstack@yahoo.com>
2019-05-23 13:30:53 -0400

    cleanup unneeded files

---
+++ nsapple/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController2.swift
+//  nsapple
+//
+//  Created by Kenneth Stack on 7/21/15.
+//  Copyright (c) 2015 Perceptus.org. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class ViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
